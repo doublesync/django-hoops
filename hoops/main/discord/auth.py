527e1c6685e75791c9b768142fda866ff1eda15f
christopher.carlton <chriscarlton368@gmail.com>
2023-01-22 17:53:25 -0500

    Added guild api call

--- hoops/main/discord/auth.py
+++ hoops/main/discord/auth.py
@@ -19,8 +19,16 @@ def exchange_code(code):
     response = requests.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
     credentials = response.json()
     acess_token = credentials["access_token"]
-    response = requests.get("https://discord.com/api/users/@me", headers={
+    # Get user info
+    user_response = requests.get("https://discord.com/api/users/@me", headers={
         "Authorization": f"Bearer {acess_token}"
     })
-    user = response.json()
-    return user
+    
+    # Get guilds info
+    guilds_response = requests.get("https://discord.com/api/users/@me/guilds", headers={
+        "Authorization": f"Bearer {acess_token}"
+    })
+    # Return info
+    user = user_response.json()
+    guilds = guilds_response.json()
+    return [user, guilds]
