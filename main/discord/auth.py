67686381ed94186e80fc6f813f82f3124bb408a6
christopher.carlton <chriscarlton368@gmail.com>
2023-03-10 18:39:20 -0500

    Fixed variable typo.

--- main/discord/auth.py
+++ main/discord/auth.py
@@ -20,17 +20,17 @@ def exchange_code(code):
         "https://discord.com/api/oauth2/token", data=data, headers=headers
     )
     credentials = response.json()
-    acess_token = credentials["access_token"]
+    access_token = credentials["access_token"]
     # Get user info
     user_response = requests.get(
         "https://discord.com/api/users/@me",
-        headers={"Authorization": f"Bearer {acess_token}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
 
     # Get guilds info
     guilds_response = requests.get(
         "https://discord.com/api/users/@me/guilds",
-        headers={"Authorization": f"Bearer {acess_token}"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     # Return info
     user = user_response.json()
