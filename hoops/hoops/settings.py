0bbdad47eac39a03acdb775d8cabb77f116582ec
christopher.carlton <chriscarlton368@gmail.com>
2023-03-10 03:10:54 -0500

    Testing

--- hoops/hoops/settings.py
+++ hoops/hoops/settings.py
@@ -1,4 +1,5 @@
-import os
+import os, sys, dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 """
 Django settings for hoops project.
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lh-iea3=ld+k6avg&!g0$o(vd2=5oc)pvu&*429wh-iqtya6l7'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 AUTHENTICATION_BACKENDS = [
     "main.authorize.DiscordBackend",
@@ -88,17 +89,21 @@ WSGI_APPLICATION = 'hoops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'baqhdzcb',
-        'USER': 'baqhdzcb',
-        'PASSWORD': 'V9hLdgs0WDMBj3Cu-87NY0GprzWDY-D_',
-        'HOST': 'salt.db.elephantsql.com',
-        'PORT': '5432',
-    }
-}
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -135,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
