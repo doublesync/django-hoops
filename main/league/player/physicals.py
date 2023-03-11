0a912936d08b0d8c38f740543565d354a4dc0aef
christopher.carlton <chriscarlton368@gmail.com>
2023-03-10 22:20:25 -0500

    Physicals work off of starting attributes.

--- main/league/player/physicals.py
+++ main/league/player/physicals.py
@@ -3,6 +3,7 @@ from ...league import config as league_config
 
 min_max_heights = league_config.min_max_heights
 min_max_weights = league_config.min_max_weights
+start_attribute = league_config.start_attribute
 
 # Finds the correct attribute limits for the player
 def setStartingPhysicals(player):
@@ -17,10 +18,10 @@ def setStartingPhysicals(player):
     height_mult = (height - minimum_height)
     weight_mult = ((weight - minimum_weight) / 2)
     # Update the physicals
-    player.attributes["Speed"] -= int(3 * height_mult)
-    player.attributes["Speed With Ball"] -= int(3 * height_mult)
-    player.attributes["Vertical"] -= int(1 * height_mult)
-    player.attributes["Strength"] += int(1 * weight_mult)
-    player.attributes["Acceleration"] -= int(1 * weight_mult)
+    player.attributes["Speed"] == start_attribute - (int(3 * height_mult))
+    player.attributes["Speed With Ball"] = start_attribute -  (int(3 * height_mult))
+    player.attributes["Vertical"] = start_attribute - int(height_mult)
+    player.attributes["Strength"] = start_attribute - int(weight_mult)
+    player.attributes["Acceleration"] = start_attribute - int(weight_mult)
     # Return the updated player
     return player
