d63d887e300c5b4ae4585a263756b73aee47a443
christopher.carlton <chriscarlton368@gmail.com>
2023-03-11 02:17:16 -0500

    Fixed physicals, they are now accurate.

--- main/league/player/physicals.py
+++ main/league/player/physicals.py
@@ -4,24 +4,29 @@ from ...league import config as league_config
 min_max_heights = league_config.min_max_heights
 min_max_weights = league_config.min_max_weights
 start_attribute = league_config.start_attribute
+position_attributes = league_config.position_starting_attributes
 
 # Finds the correct attribute limits for the player
 def setStartingPhysicals(player):
     # Find the player details
-    physicals = league_config.attribute_categories["physical"]
     height = int(player.height)
     weight = int(player.weight)
     position = player.primary_position
     # Find height & weight multipliers
     minimum_height = min_max_heights[position]["min"]
     minimum_weight = min_max_weights[position]["min"]
-    height_mult = (height - minimum_height)
-    weight_mult = ((weight - minimum_weight) / 2)
-    # Update the physicals
-    player.attributes["Speed"] == start_attribute - (int(3 * height_mult))
-    player.attributes["Speed With Ball"] = start_attribute -  (int(3 * height_mult))
-    player.attributes["Vertical"] = start_attribute - int(height_mult)
-    player.attributes["Strength"] = start_attribute - int(weight_mult)
-    player.attributes["Acceleration"] = start_attribute - int(weight_mult)
+    height_mult = height - minimum_height
+    weight_mult = round((weight - minimum_weight) / 2)
+    # Calculate the player's physicals
+    speed_start = position_attributes[position]["Speed"]
+    vertical_start = position_attributes[position]["Vertical"]
+    strength_start = position_attributes[position]["Strength"]
+    acceleration_start = position_attributes[position]["Acceleration"]
+    # Update the player's physicals
+    player.attributes["Speed"] = speed_start - (int(3 * height_mult))
+    player.attributes["Speed With Ball"] = speed_start - (int(3 * height_mult))
+    player.attributes["Vertical"] = vertical_start - int(height_mult)
+    player.attributes["Strength"] = strength_start + int(weight_mult)
+    player.attributes["Acceleration"] = acceleration_start - int(weight_mult)
     # Return the updated player
     return player
