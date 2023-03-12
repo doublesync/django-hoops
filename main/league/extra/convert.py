a0b5a42db43a11d79851a9b6d9e4a766f52992e1
christopher.carlton <chriscarlton368@gmail.com>
2023-03-12 08:12:13 -0400

    Added DB --> DjangoForm converters (lists & dicts)

--- main/league/extra/convert.py
+++ main/league/extra/convert.py
@@ -2,3 +2,17 @@ game_friendly = lambda a: (a - 25) * 3
 user_friendly = lambda a: (a / 3) + 25
 
 convert_to_height = lambda x: str(int(x // 12)) + "'" + str(int(x % 12))
+
+
+def format_dict_for_django_forms(dictionary):
+    dictionary = dictionary.copy()
+    step_1 = {k.lower(): v for k, v in dictionary.items()}
+    step_2 = {k.replace(" ", "_"): v for k, v in step_1.items()}
+    return step_2
+
+
+def format_list_for_django_forms(list):
+    list = list.copy()
+    step_1 = [x.lower() for x in list]
+    step_2 = [x.replace(" ", "_") for x in step_1]
+    return step_2
--- main/myTest.py
+++ main/myTest.py
@@ -1,2 +1,13 @@
 from .models import Player
-from .league.player import contract
+from .league.player import upgrade
+
+
+def main():
+    p = Player.objects.get(pk=1)
+    c = upgrade.attributeCost(p, "Driving Layup", 60, 99)
+
+    print("Total Cost:" + str(c))
+
+
+if __name__ == "__main__":
+    main()
