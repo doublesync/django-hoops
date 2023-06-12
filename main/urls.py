1da0f3f4ab233a1d244ce10d7890aa4337ac10e5
christopher.carlton <chriscarlton368@gmail.com>
2023-06-12 07:33:03 -0400

    Organized url files for stats & main apps.

--- main/urls.py
+++ main/urls.py
@@ -2,166 +2,67 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Home & login PATHS
-    path(route="", view=views.home, name="home"),
-    path(route="login/", view=views.login, name="login"),
-    path(route="login/discord/", view=views.login_discord, name="login_discord"),
-    path(
-        route="login/discord/redirect/",
-        view=views.login_discord_redirect,
-        name="login_discord_redirect",
-    ),
-    path(route="logout/", view=views.logout, name="logout"),
-    # Player PATHS
-    path(route="player/<int:id>/", view=views.player, name="player"),
-    path(
-        route="player/upgrade/<int:id>/",
-        view=views.upgrade_player,
-        name="upgrade_player",
-    ),
-    path(route="player/create/", view=views.create_player, name="create_player"),
-    path(route="players/", view=views.players, name="players"),
-    path(route="logs/upgrades/<int:id>/", view=views.upgrade_logs, name="upgrade_logs"),
-    path(route="logs/cash/<int:id>/", view=views.cash_logs, name="cash_logs"),
-    path(route="players/builder/", view=views.mock_builder, name="mock_builder"),
-    path(
-        route="players/vitals/update/<int:id>/",
-        view=views.update_player_vitals,
-        name="update_player_vitals",
-    ),
-    path(
-        route="players/pending/update/",
-        view=views.update_player_pending_upgrades,
-        name="update_player_pending_upgrades",
-    ),
-    path(
-        route="player/physicals/edit/<int:id>/",
-        view=views.edit_physicals,
-        name="edit_physicals",
-    ),
-    path(route="players/free-agents/", view=views.free_agents, name="free_agents"),
-    path(route="player/styles/<int:id>/", view=views.player_styles, name="player_styles"),
-    # Player Cash PATHS
-    path(route="player/cash/add/", view=views.add_player_cash, name="add_player_cash"),
-    path(
-        route="player/cash/take/", view=views.take_player_cash, name="take_player_cash"
-    ),
-    path(
-        route="player/daily/reward/collect/",
-        view=views.daily_rewards,
-        name="daily_rewards",
-    ),
-    # Team PATHS
-    path(route="team/<int:id>/", view=views.team, name="team"),
-    path(route="teams/", view=views.teams, name="teams"),
-    path(route="team/trade/", view=views.trade, name="trade"),
-    path(
-        route="team/trade/accept/<int:id>/",
-        view=views.accept_trade,
-        name="accept_trade",
-    ),
-    path(
-        route="team/trade/decline/<int:id>/",
-        view=views.decline_trade,
-        name="decline_trade",
-    ),
-    path(
-        route="trades/panel/",
-        view=views.trade_panel,
-        name="trade_panel",
-    ),
-    path(
-        route="team/trade/finalize/",
-        view=views.check_finalize_trade,
-        name="check_finalize_trade",
-    ),
-    # Misc PATHS
-    path(
-        route="upgrades/pending/", view=views.upgrades_pending, name="upgrades_pending"
-    ),
-    path(route="frivolities/", view=views.frivolities, name="frivolities"),
-    path(route="coupons/", view=views.coupons, name="coupons"),
-    # Check PATHS
-    path(
-        route="players/search/",
-        view=views.check_player_search,
-        name="check_player_search",
-    ),
-    path(route="teams/search/", view=views.check_team_search, name="check_team_search"),
-    path(
-        route="coupons/redeem/", view=views.check_coupon_code, name="check_coupon_code"
-    ),
-    path(
-        route="keys/redeem/", view=views.check_license_key, name="check_license_key"
-    ),
-    path(
-        route="players/attributes/",
-        view=views.check_starting_attributes,
-        name="check_starting_attributes",
-    ),
-    path(
-        route="positions/count/",
-        view=views.check_position_count,
-        name="check_position_count",
-    ),
-    path(
-        route="upgrades/validate/",
-        view=views.check_upgrade_validation,
-        name="check_upgrade_validation",
-    ),
-    path(
-        route="players/leaders/",
-        view=views.check_player_leaders,
-        name="check_player_leaders",
-    ),
-    path(
-        route="metas/leaders/",
-        view=views.check_meta_leaders,
-        name="check_meta_leaders",
-    ),
-    path(
-        route="attributes/leaders/",
-        view=views.check_attribute_leaders,
-        name="check_attribute_leaders",
-    ),
-    path(
-        route="teams/roster/",
-        view=views.check_team_roster,
-        name="check_team_roster",
-    ),
-    path(
-        route="teams/trade/",
-        view=views.check_trade_validation,
-        name="check_trade_validation",
-    ),
-    path(
-        route="notifications/mark/read/",
-        view=views.check_read_notification,
-        name="check_read_notification",
-    ),
-    path(
-        route="player/daily/reward/",
-        view=views.check_daily_reward,
-        name="check_daily_reward",
-    ),
-    path(
-        route="player/weight/check/",
-        view=views.check_weight_change,
-        name="check_weight_change",
-    ),
-    path(
-        route="players/free-agents/search/",
-        view=views.check_free_agent_search,
-        name="check_free_agent_search",
-    ),
-    path(route="contract/offer/", view=views.check_contract_offer, name="check_contract_offer"),
-    path(route="contract/revoke/", view=views.check_contract_revoke, name="check_contract_revoke"),
-    # User PATHS
-    path(
-        route="user/notifications/<int:id>/",
-        view=views.notifications,
-        name="notifications",
-    ),
-    # Ad PATHS
+    
+    # Discord URL patterns
+    path("", views.home, name="home"),
+    path("login/", views.login, name="login"),
+    path("login/discord/", views.login_discord, name="login_discord"),
+    path("login/discord/redirect/", views.login_discord_redirect, name="login_discord_redirect"),
+    path("logout/", views.logout, name="logout"),
+
+    # Player URL patterns
+    path("player/<int:id>/", views.player, name="player"),
+    path("player/upgrade/<int:id>/", views.upgrade_player, name="upgrade_player"),
+    path("player/create/", views.create_player, name="create_player"),
+    path("players/", views.players, name="players"),
+    path("logs/upgrades/<int:id>/", views.upgrade_logs, name="upgrade_logs"),
+    path("logs/cash/<int:id>/", views.cash_logs, name="cash_logs"),
+    path("players/builder/", views.mock_builder, name="mock_builder"),
+    path("players/vitals/update/<int:id>/", views.update_player_vitals, name="update_player_vitals"),
+    path("players/pending/update/", views.update_player_pending_upgrades, name="update_player_pending_upgrades"),
+    path("player/physicals/edit/<int:id>/", views.edit_physicals, name="edit_physicals"),
+    path("players/free-agents/", views.free_agents, name="free_agents"),
+    path("player/styles/<int:id>/", views.player_styles, name="player_styles"),
+    path("player/cash/add/", views.add_player_cash, name="add_player_cash"),
+    path("player/cash/take/", views.take_player_cash, name="take_player_cash"),
+    path("player/daily/reward/collect/", views.daily_rewards, name="daily_rewards"),
+
+    # Team URL patterns
+    path("team/<int:id>/", views.team, name="team"),
+    path("teams/", views.teams, name="teams"),
+    path("team/trade/", views.trade, name="trade"),
+    path("team/trade/accept/<int:id>/", views.accept_trade, name="accept_trade"),
+    path("team/trade/decline/<int:id>/", views.decline_trade, name="decline_trade"),
+    path("trades/panel/", views.trade_panel, name="trade_panel"),
+    path("team/trade/finalize/", views.check_finalize_trade, name="check_finalize_trade"),
+
+    # Miscellaneous URL patterns
+    path("upgrades/pending/", views.upgrades_pending, name="upgrades_pending"),
+    path("frivolities/", views.frivolities, name="frivolities"),
+    path("coupons/", views.coupons, name="coupons"),
     path("ads.txt", views.ad_view.as_view()),
+
+    # Htmx URL patterns
+    path("players/search/", views.check_player_search, name="check_player_search"),
+    path("teams/search/", views.check_team_search, name="check_team_search"),
+    path("coupons/redeem/", views.check_coupon_code, name="check_coupon_code"),
+    path("keys/redeem/", views.check_license_key, name="check_license_key"),
+    path("players/attributes/", views.check_starting_attributes, name="check_starting_attributes"),
+    path("positions/count/", views.check_position_count, name="check_position_count"),
+    path("upgrades/validate/", views.check_upgrade_validation, name="check_upgrade_validation"),
+    path("players/leaders/", views.check_player_leaders, name="check_player_leaders"),
+    path("metas/leaders/", views.check_meta_leaders, name="check_meta_leaders"),
+    path("attributes/leaders/", views.check_attribute_leaders, name="check_attribute_leaders"),
+    path("teams/roster/", views.check_team_roster, name="check_team_roster"),
+    path("teams/trade/", views.check_trade_validation, name="check_trade_validation"),
+    path("notifications/mark/read/", views.check_read_notification, name="check_read_notification"),
+    path("player/daily/reward/", views.check_daily_reward, name="check_daily_reward"),
+    path("player/weight/check/", views.check_weight_change, name="check_weight_change"),
+    path("players/free-agents/search/", views.check_free_agent_search, name="check_free_agent_search"),
+    path("contract/offer/", views.check_contract_offer, name="check_contract_offer"),
+    path("contract/revoke/", views.check_contract_revoke, name="check_contract_revoke"),
+
+    # User URL patterns
+    path("user/notifications/<int:id>/", views.notifications, name="notifications"),
+
 ]
--- stats/urls.py
+++ stats/urls.py
@@ -2,22 +2,19 @@ from django.urls import path
 from . import views
 
 app_name = 'stats'
-urlpatterns = []
+urlpatterns = [
 
-mainurls = [
+    # Main URL patterns
     path('', views.index, name='index'),
     path('games/add/', views.add_game, name='add_game'),
     path('games/view/<int:id>/', views.view_game, name='view_game'),
     path('seasons/view/<int:id>/', views.view_season, name='view_season'),
     path('seasons/averages/view/<int:id>/', views.view_season_stats, name='view_season_stats'),
-]
 
-htmxurls = [
+    # HTMX URL patterns
     path('roster/', views.check_stats_roster, name='check_stats_roster'),
     path('games/add/validate/', views.validate_game, name='validate_game'),
     path('seasons/averages/sort/', views.sort_stats, name='sort_stats'),
     path('seasons/averages/options/', views.find_options, name='find_options'),
-]
 
-urlpatterns += mainurls
-urlpatterns += htmxurls
+]
