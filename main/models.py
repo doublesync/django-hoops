af9e303389966afb2f875ee53621b112fc4a3e88
christopher.carlton <chriscarlton368@gmail.com>
2023-07-13 18:01:19 -0400

    Added discord_user permissions.

--- main/models.py
+++ main/models.py
@@ -25,6 +25,8 @@ class DiscordUser(models.Model):
     can_update_players = models.BooleanField(default=False)
     can_approve_trades = models.BooleanField(default=False)
     can_update_styles = models.BooleanField(default=False)
+    can_edit_events = models.BooleanField(default=False)
+    can_edit_scores = models.BooleanField(default=False)
     # Player Slots
     player_slots = models.SmallIntegerField(default=league_config.max_players)
     auto_collect_rewards = models.BooleanField(default=False)
