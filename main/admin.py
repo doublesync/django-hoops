d20bf0d2d5f82f8995e71e16b6119206ccabca3c
christopher.carlton <chriscarlton368@gmail.com>
2023-08-29 09:31:24 -0400

    Removed admin logs (django has them)

--- main/admin.py
+++ main/admin.py
@@ -30,48 +30,48 @@ class MyAdmin(admin.ModelAdmin):
         JSONField: {"widget": JSONEditor},
     }
 
-    def save_model(self, request, obj, form, change):
-        # Get the original object from the database
-        original_obj = self.model.objects.get(pk=obj.pk)
+    # def save_model(self, request, obj, form, change):
+    #     # Get the original object from the database
+    #     original_obj = self.model.objects.get(pk=obj.pk)
 
-        # Compare the fields to find the changes
-        changed_fields = []
-        for field in obj._meta.fields:
-            if getattr(original_obj, field.name) != getattr(obj, field.name):
-                changed_fields.append(field.name)
+    #     # Compare the fields to find the changes
+    #     changed_fields = []
+    #     for field in obj._meta.fields:
+    #         if getattr(original_obj, field.name) != getattr(obj, field.name):
+    #             changed_fields.append(field.name)
 
-        # Print the message with the updated fields
-        changed_fields_str = ""
-        for field in changed_fields:
-            changed_fields_str += f"{request.user.username} | {obj} | {field} | {original_obj.__dict__[field]} -> **{obj.__dict__[field]}**\n"
+    #     # Print the message with the updated fields
+    #     changed_fields_str = ""
+    #     for field in changed_fields:
+    #         changed_fields_str += f"{request.user.username} | {obj} | {field} | {original_obj.__dict__[field]} -> **{obj.__dict__[field]}**\n"
 
-        # Prepping a link using the PastebinAPI
-        api_endpoint = "https://pastebin.com/api/api_post.php"
-        data = {
-            # ZW38KqwFTFvgvHUgcWE3cFV41ID-WHUW
-            # omc_XJIUCPgz1QrHSxK55AaQ5juwJg-p
-            "api_dev_key": "omc_XJIUCPgz1QrHSxK55AaQ5juwJg-p",
-            "api_option": "paste",
-            "api_paste_code": "N/A",
-            "api_paste_expire_data": "1H",
-            "api_paste_name": "N/A"
-        }
+    #     # Prepping a link using the PastebinAPI
+    #     api_endpoint = "https://pastebin.com/api/api_post.php"
+    #     data = {
+    #         # ZW38KqwFTFvgvHUgcWE3cFV41ID-WHUW
+    #         # omc_XJIUCPgz1QrHSxK55AaQ5juwJg-p
+    #         "api_dev_key": "omc_XJIUCPgz1QrHSxK55AaQ5juwJg-p",
+    #         "api_option": "paste",
+    #         "api_paste_code": "N/A",
+    #         "api_paste_expire_data": "1H",
+    #         "api_paste_name": "N/A"
+    #     }
 
-        # Create a link using the PastebinAPI
-        data["api_paste_code"] = changed_fields_str
-        data["api_paste_name"] = "Admin Change Log"
-        rq = requests.post(url=api_endpoint, data=data)
-        link = rq.text
+    #     # Create a link using the PastebinAPI
+    #     data["api_paste_code"] = changed_fields_str
+    #     data["api_paste_name"] = "Admin Change Log"
+    #     rq = requests.post(url=api_endpoint, data=data)
+    #     link = rq.text
 
-        # Send the webhook
-        discord_webhooks.send_webhook(
-            url="panel_logs",
-            title="🔎 Panel Log",
-            message=f"Admin {request.user.username} made changes to the {obj._meta.verbose_name} {obj}. \nChanged fields: \n{changed_fields_str} \n[View Change Log]({link})",
-        )
+    #     # Send the webhook
+    #     discord_webhooks.send_webhook(
+    #         url="panel_logs",
+    #         title="🔎 Panel Log",
+    #         message=f"Admin {request.user.username} made changes to the {obj._meta.verbose_name} {obj}. \nChanged fields: \n{changed_fields_str} \n[View Change Log]({link})",
+    #     )
 
-        # Call the parent class's save_model method to actually save the object
-        super().save_model(request, obj, form, change)
+    #     # Call the parent class's save_model method to actually save the object
+    #     super().save_model(request, obj, form, change)
 
 # Register your models here.
 admin.site.register(DiscordUser, MyAdmin)
