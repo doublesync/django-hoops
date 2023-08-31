d3ce83c9f8d0a21736f4e9d66ebfd5852fa8bfe6
christopher.carlton <chriscarlton368@gmail.com>
2023-08-31 14:04:58 -0400

    Added home page information and login redirect

--- wrestling/views.py
+++ wrestling/views.py
@@ -1,16 +1,38 @@
+# Django imports
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Main imports
+from main.models import DiscordUser
+
+# Python imports
+import os
+
+# Custom imports
+from .models import Wrestler
+from .models import Team
+from .models import Show
 
 # Create your views here.
+@login_required(login_url="/login/discord/")
 def index(request):
-    return render(request, 'wrestling/index.html')
+    # Find the user's wrestlers
+    wrestlers = Wrestler.objects.filter(manager=request.user)
+    context = {
+        "wrestlers": wrestlers,
+        "motd": os.environ.get("MOTD"),
+    }
+    return render(request, 'wrestling/league/home.html', context=context)
 
 # Wrestler views
+@login_required(login_url="/login/discord/")
 def wrestler_create(request):
     return render(request, 'wrestling/wrestler_create.html')
 
 def wrestler_profile(request, wrestler_id):
     return render(request, 'wrestling/wrestler_profile.html')
 
+@login_required(login_url="/login/discord/")
 def wrestler_upgrade(request, wrestler_id):
     return render(request, 'wrestling/wrestler_upgrade.html')
 
